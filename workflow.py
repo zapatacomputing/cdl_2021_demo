f795fb7b19b98eb00ba0eacc343979f2da1fcce7
mstechly <michal.stechly@gmail.com>
2021-07-15 16:56:59 +0200

    Updated workflow.

--- workflow.py
+++ workflow.py
@@ -1,51 +1,72 @@
 import logging
-from typing import Callable, List, Optional
+import os
+from typing import List, Optional, Dict
+import networkx as nx
+
 import numpy as np
-from openfermion.ops.operators.qubit_operator import QubitOperator
-from zquantum.core.interfaces.ansatz import Ansatz
-from zquantum.core.interfaces.backend import QuantumBackend
+import pandas as pd
+from openfermion import QubitOperator
 import qe.sdk.v1 as qe
-from qequlacs.simulator import QulacsSimulator
+from qequlacs import QulacsSimulator
 from zquantum.core.interfaces.optimizer import Optimizer, OptimizeResult
-from zquantum.core.estimation import calculate_exact_expectation_values
+
+from zquantum.core.interfaces.ansatz import Ansatz
 from zquantum.core.cost_function import AnsatzBasedCostFunction
-from zquantum.core.graph import generate_random_regular_graph
+from zquantum.core.estimation import calculate_exact_expectation_values
+from zquantum.core.graph import (
+    generate_graph_from_specs,
+    load_graph,
+)
+from zquantum.core.interfaces.backend import QuantumBackend
+from zquantum.core.interfaces.optimizer import (
+    Optimizer,
+    optimization_result,
+)
 from zquantum.optimizers.scipy_optimizer import ScipyOptimizer
 from zquantum.qaoa.ansatzes.farhi_ansatz import QAOAFarhiAnsatz
 from zquantum.qaoa.problems.maxcut import (
     get_maxcut_hamiltonian as _get_maxcut_hamiltonian,
 )
-from qequlacs import QulacsSimulator
-import networkx as nx
-from openfermion import QubitOperator
+
+
+def load_graph_from_file(graph_size, graph_id):
+    return load_graph(
+        os.path.join(
+            "graphs", f"size_{graph_size}", f"graph_{graph_size}_{graph_id}.json"
+        )
+    )
 
 
 @qe.step(
     resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
 )
-def generate_graph(size: int, degree: int) -> nx.Graph:
-    return generate_random_regular_graph(size, degree)
+def get_graph(size_of_graph: int, graph_id: int, graph_specs: Dict) -> nx.Graph:
+    if graph_specs is None:
+        return load_graph_from_file(size_of_graph, graph_id)
+    graph_specs["num_nodes"] = size_of_graph
+    return generate_graph_from_specs(graph_specs)
 
 
 @qe.step(
     resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
 )
 def get_maxcut_hamiltonian(graph: nx.Graph) -> QubitOperator:
-    # return QubitOperator("2*[Z0 Z1]")
     return _get_maxcut_hamiltonian(graph)
 
 
 @qe.step(
     resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
 )
-def get_ansatz(n_layers: int, cost_hamiltonian: QubitOperator) -> Ansatz:
-    return QAOAFarhiAnsatz(number_of_layers=n_layers, cost_hamiltonian=cost_hamiltonian)
+def get_ansatz(number_of_layers: int, cost_hamiltonian: QubitOperator) -> Ansatz:
+    return QAOAFarhiAnsatz(
+        number_of_layers=number_of_layers, cost_hamiltonian=cost_hamiltonian
+    )
 
 
 @qe.step(
     resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
 )
-def generate_random_initial_parameters(
+def generate_random_parameters(
     min_value: float, max_value: float, number_of_parameters: int
 ) -> np.ndarray:
     return np.random.uniform(min_value, max_value, number_of_parameters)
@@ -54,22 +75,67 @@ def generate_random_initial_parameters(
 @qe.step(
     resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
 )
-def optimize_circuit(
-    ansatz: QAOAFarhiAnsatz,
+def get_params_from_results(results: OptimizeResult) -> np.ndarray:
+    return results.opt_params
+
+
+@qe.step(
+    resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
+)
+def find_appropriate_params(
     cost_hamiltonian: QubitOperator,
+    ansatz: Ansatz,
+    initial_params: np.ndarray,
+    mode: str,
     backend: QuantumBackend,
     optimizer: Optimizer,
-    initial_parameters: np.ndarray,
-    estimation_method: Callable,
 ) -> OptimizeResult:
+    if mode == "low":
+        cost_function = AnsatzBasedCostFunction(
+            target_operator=-cost_hamiltonian,
+            ansatz=ansatz,
+            backend=backend,
+            estimation_method=calculate_exact_expectation_values,
+        )
+    else:
+        cost_function = AnsatzBasedCostFunction(
+            target_operator=cost_hamiltonian,
+            ansatz=ansatz,
+            backend=backend,
+            estimation_method=calculate_exact_expectation_values,
+        )
 
-    cost_function = AnsatzBasedCostFunction(
-        target_operator=cost_hamiltonian,
-        ansatz=ansatz,
-        backend=backend,
-        estimation_method=estimation_method,
-    )
-    return optimizer.minimize(cost_function, initial_parameters)
+    if mode in {"random", "evaluate"}:
+        opt_results = optimization_result(
+            opt_value=cost_function(initial_params),
+            opt_params=initial_params,
+        )
+    else:
+        opt_results = optimizer.minimize(cost_function, initial_params)
+
+    if mode == "low":
+        opt_results.opt_value = -opt_results.opt_value
+    return opt_results
+
+
+@qe.step(
+    resource_def=qe.ResourceDefinition(cpu="1000m", memory="2Gi", disk="10Gi"),
+)
+def analyze_evaluations(
+    number_of_layers: int, evaluation_results: List  # Dict
+) -> pd.DataFrame:
+    df = pd.DataFrame(columns=["n_layers", "mode", "mean", "std"])
+    for mode, results_list in evaluation_results.items():
+        values = [result.opt_value for result in results_list]
+        df = df.append(
+            {
+                "n_layers": number_of_layers,
+                "mode": mode,
+                "mean": np.mean(values),
+                "std": np.std(values),
+            }
+        )
+    return df
 
 
 @qe.workflow(
@@ -82,52 +148,101 @@ def optimize_circuit(
         qe.GitImportDefinition.get_current_repo_and_branch(),
     ],
 )
-def qaoa_workflow(
+def qaoa_concentration_workflow(
     size_of_graph: int,
+    size_of_big_graph: int,
     number_of_graphs: int,
-    number_of_optimization_runs: int,
+    min_layers: int,
+    max_layers: int,
+    modes: List[str],
+    graph_specs: Optional[Dict],
     backend: QuantumBackend,
     optimizer: Optimizer,
-    seed: Optional[int] = None,
 ) -> List[qe.StepDefinition]:
-    assert size_of_graph % 2 == 0
-    if seed is not None:
-        np.random.seed(seed)
 
-    all_runs = []
-    for graph_id in range(number_of_graphs):
-        graph = generate_graph(size_of_graph, 3)
+    ####################
+    # 2. Layers loop
+    ####################
+    all_results = []
+    for number_of_layers in range(min_layers, max_layers + 1):
+        ####################
+        # 3. Problem definition
+        ####################
+        graph_id = 0
+        graph = get_graph(size_of_graph, graph_id, graph_specs)
         cost_hamiltonian = get_maxcut_hamiltonian(graph)
-        ansatz = get_ansatz(2, cost_hamiltonian)
-        number_of_params = 4
-        all_runs += [graph, cost_hamiltonian, ansatz]
-        for _ in range(number_of_optimization_runs):
-            all_runs.append(
-                optimize_circuit(
-                    ansatz,
-                    cost_hamiltonian,
-                    backend,
-                    optimizer,
-                    generate_random_initial_parameters(
-                        -np.pi / 2, np.pi / 2, number_of_params
-                    ),
-                    calculate_exact_expectation_values,
-                )
+        ansatz = get_ansatz(
+            number_of_layers=number_of_layers, cost_hamiltonian=cost_hamiltonian
+        )
+
+        ####################
+        # 4. Finding parameters
+        ####################
+        selected_results = []
+        for mode in modes:
+            number_of_params = 2 * number_of_layers
+            initial_params = generate_random_parameters(-np.pi, np.pi, number_of_params)
+            opt_results = find_appropriate_params(
+                cost_hamiltonian, ansatz, initial_params, mode, backend, optimizer
             )
-    return all_runs
+            selected_results.append(opt_results)
+        ####################
+        # 5. Evaluating parameters
+        ####################
+        for evaluation_size in [size_of_graph, size_of_big_graph]:
+            # execution_results = {mode: [] for mode in modes}
+            execution_results = []
+            for graph_id in range(number_of_graphs):
+                ####################
+                # 5.1 Problem definition
+                ####################
+
+                graph = get_graph(evaluation_size, graph_id, graph_specs)
+                cost_hamiltonian = get_maxcut_hamiltonian(graph)
+                ansatz = get_ansatz(
+                    number_of_layers=number_of_layers, cost_hamiltonian=cost_hamiltonian
+                )
+                ####################
+                # 5.2 Execution
+                ####################
+                for mode, results in zip(modes, selected_results):
+                    params = get_params_from_results(results)
+                    opt_results = find_appropriate_params(
+                        cost_hamiltonian, ansatz, params, "evaluate", backend, optimizer
+                    )
+                    # execution_results[mode].append(opt_results)
+                    all_results.append(opt_results)
+
+            # ####################
+            # # 6. Data presentation
+            # ####################
+            analysis_results = analyze_evaluations(number_of_layers, execution_results)
+            # all_results.append(analysis_results)
+    return all_results
 
 
 if __name__ == "__main__":
-    rng_seed = 9
-    # graph = generate_random_regular_graph(4, 3)
-    # hamiltonian = _get_maxcut_hamiltonian(graph)
-    wf: qe.WorkflowDefinition = qaoa_workflow(
-        size_of_graph=4,
-        number_of_graphs=2,
-        number_of_optimization_runs=2,
+    ####################
+    # 1. Simulation parameters
+    ####################
+    size_of_graph = 10
+    size_of_big_graph = 20
+    number_of_graphs = 2
+    min_layers = 2
+    max_layers = 2
+    modes = ["high", "random", "low"]
+    graph_specs = {"type_graph": "regular", "degree": 3}
+
+    wf: qe.WorkflowDefinition = qaoa_concentration_workflow(
+        size_of_graph=size_of_graph,
+        size_of_big_graph=size_of_big_graph,
+        number_of_graphs=number_of_graphs,
+        min_layers=min_layers,
+        max_layers=max_layers,
+        modes=modes,
+        graph_specs=graph_specs,
         backend=QulacsSimulator(),
         optimizer=ScipyOptimizer(method="L-BFGS-B"),
-        seed=rng_seed,
     )
 
     # result = wf.local_run(log_level=logging.INFO)
@@ -135,5 +250,4 @@ if __name__ == "__main__":
 
     # pdb.set_trace()
     wf.validate()
-    # wf.print_workflow()
     wf.submit()
